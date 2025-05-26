e3e703afb33150948f2274a1f319004cc668d74a
famim2002 <famimhayatyash@gmail.com>
2025-05-27 00:27:20 +0600

    redux-installation

---
+++ src/components/DecrementBtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DecrementBtn = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DecrementBtn
---
+++ src/components/IncrementBtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IncrementBtn = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default IncrementBtn
